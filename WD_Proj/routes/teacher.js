5a2774fb7372e9a78c2e974deef332fd22d945c5
sebastianzoolander <mmuaz.arif21@lyceumschool.edu.pk>
2024-03-14 00:47:03 +0500

    CRUD operations

--- WD_Proj/routes/teacher.js
+++ WD_Proj/routes/teacher.js
@@ -6,6 +6,17 @@ var express = require("express");
 var router = express.Router();
 
 
+router.post("/getSlotsbySectionId", async (req, res) => {
+    try{
+        const {sectionId} = req.body
+        let sv = await slot.find({sectionId: sectionId}).populate("teacher")
+        if(!sv) return res.json({msg: "No slots found"})
+        return res.json({sv})
+    }catch(error){
+        console.error(error)
+    }
+});
+
 
 router.use(async (req, res, next) => {
     if(!req.user.role == "teacher") return res.json({ msg: "NOT TEACHER" })
@@ -28,6 +39,37 @@ router.post("/createSlot", async (req, res) => {
         console.error(error)
     }
 });
+
+router.post("/deleteSlot", async (req, res) => {
+    try {
+        const { sectionId } = req.body;
+        //console.log("User:", req.user);
+        let user = await teacher.findOne({ email: req.user.email });
+        let sv = await slot.findOne({ sectionId: sectionId, teacher: user._id});
+        if (!sv) return res.json({ msg: "Slot not found or you are not authorized to delete it" });
+        await slot.deleteOne({ sectionId: sectionId, teacher: user._id});
+        return res.json({ msg: "Slot deleted successfully" });
+    } catch (error) {
+        console.error(error);
+    }
+
+})
+
+router.post("/updateSlot", async (req, res) => {
+    try {
+        const { sectionId, updatedsectionId } = req.body;  // over here only section updated later when front end is ready we can add more fields
+        let user = await teacher.findOne({ email: req.user.email });
+        let sv = await slot.findOne({ sectionId: sectionId, teacher: user._id });
+        if (!sv) return res.json({ msg: "Slot not found or you are not authorized to update it" });
+        await slot.updateOne({ sectionId: updatedsectionId});
+        return res.json({ msg: "Slot updated successfully" });
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+
+
 
 module.exports = router;
 
