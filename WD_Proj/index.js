329d23b7783862451fa31db4b33a20fcefaadf96
sebastianzoolander <mmuaz.arif21@lyceumschool.edu.pk>
2024-03-22 11:49:41 +0500

    pagination

--- WD_Proj/index.js
+++ WD_Proj/index.js
@@ -28,3 +28,36 @@ app.use(function(req, res, next) { // catch 404 and forward to error handler
 
 const PORT = 5600;
 app.listen(PORT, console.log(`Server running port ${PORT}`)); // Start the server on port 5600
+
+// function paginatedResults(model) {
+//     return async (req, res, next) => {
+//       const page = parseInt(req.query.page)
+//       const limit = parseInt(req.query.limit)
+  
+//       const startIndex = (page - 1) * limit
+//       const endIndex = page * limit
+  
+//       const results = {}
+  
+//       if (endIndex < await model.countDocuments().exec()) {
+//         results.next = {
+//           page: page + 1,
+//           limit: limit
+//         }
+//       }
+      
+//       if (startIndex > 0) {
+//         results.previous = {
+//           page: page - 1,
+//           limit: limit
+//         }
+//       }
+//       try {
+//         results.results = await model.find().limit(limit).skip(startIndex).exec()
+//         res.paginatedResults = results
+//         next()
+//       } catch (e) {
+//         res.status(500).json({ message: e.message })
+//       }
+//     }
+//   }
